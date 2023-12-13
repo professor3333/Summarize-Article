3fdfab3dda3fdd672eec25f2050b42d87f54ce60
Niroj33 <86530493+professor3333@users.noreply.github.com>
2023-12-13 21:06:29 +0545

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type="button"
           onClick={() =>
-            window.open('https://github.com/TidbitsJS/Summize', '_blank')
+            window.open('https://github.com/professor3333/Summarize-Article', '_blank')
           }
           className="black_btn"
         >
